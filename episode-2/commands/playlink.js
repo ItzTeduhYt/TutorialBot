244961a75b16a4526e50448cd99b17423fe0586a
ItzTeduhYt <57523809+ItzTeduhYt@users.noreply.github.com>
2020-12-08 15:02:59 +0700

    Create playlink.js

---
+++ episode-2/commands/playlink.js
@@ -0,0 +1,16 @@
+let ytdl = require('ytdl-core')
+exports.run = async(client, message, args) => {
+  let queue = client.queue.get(message.guild.id)
+  if (!message.member.voice.channel) return message.channel.send(`Mohon masuk ke Voice Channel!`);
+  
+  if (!args[0]) return  message.channel.send(`Mohon masukan link video youtube!`);
+  
+  let validateURL = await  ytdl.validateURL(args[0])
+  if (!validateURL) return message.channel.send(`Mohon masukan link video youtube yg benar!`);
+  
+  let info = await ytdl.getInfo(args[0])
+  let connection = await message.member.voice.channel.join()
+  
+  let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+  message.channel.send(`Now Playing: ${info.videoDetails.title}!`)
+}
